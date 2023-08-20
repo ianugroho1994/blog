996fdcd2d623afea300298a6bbb681fe1171f709
ardianto nugroho <ardianto.nugroho@dispendix.com>
2023-08-20 12:20:23 +0700

    feat: update settings

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -15,8 +15,8 @@ export const MENU_LINKS = [
 
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
-	github: "https://github.com/chrismwilliams/astro-cactus",
-	twitter: "https://github.com/chrismwilliams/astro-cactus",
+	github: "https://github.com/ianugroho1994",
+	twitter: "https://twitter.com/ianugroho",
 	discord: "https://github.com/chrismwilliams/astro-cactus",
-	email: "me@example.com",
+	email: "ardianto.nugroho1994@gmail.com",
 };
--- src/site.config.ts
+++ src/site.config.ts
@@ -12,11 +12,11 @@ interface SiteConfig {
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "Ian nugroho",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "Ian Nugroho's Blog",
 	// Meta property used as a default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "just another software engineer blog",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
